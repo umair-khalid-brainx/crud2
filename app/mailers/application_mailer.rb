ce881b9c5755b2e86188d49b30fa9163c44981ba
umair-khalid-brainx <umair.khalid@brainxtech.com>
2022-11-16 16:55:26 +0500

    Minor changes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,2 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "umair.khalid.brainxtech.test@gmail.com"
-  layout "mailer"
 end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,25 @@
 class OrderMailer < ApplicationMailer
+  before_action { puts "========== BEFORE ACTION IN MAILER =========="}
+  before_action { puts "========== AFTER ACTION IN MAILER =========="}
+  default from: "umair.khalid.brainxtech.test@gmail.com"
+  layout "mailer"
   def new_order_email
     @order = params[:order]
-    mail(to: @order.email, subject: "You got a new order!")
+    # attachments.inline['image.jpg'] = File.read('app/assets/darth-vader.jpg')
+    mail(to: @order.email, subject: "You got a new order!", template_path: 'order_mailer', template_name: 'new_order_email')
     puts "========== Mail sent =========="
+  end
+
+  def receive(email)
+    order = Order.find_by(address: email.to.first)
+    page.emails.create(
+      subject: email.subject,
+      body: email.body
+    )
+    if email.has_attachments?
+      email.attachments.each do |attachment|
+        page.attachments.create({ file: attachment, description: email.subject })
+      end
+    end
   end
 end
